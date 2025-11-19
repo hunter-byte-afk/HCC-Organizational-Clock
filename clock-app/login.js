1ecafa8671e0dea8bbc077fd3f05322ef083148b
VictoriaTrev <vrtrevin@nmsu.edu>
2025-11-19 09:15:16 -0700

    working on the calClock

--- clock-app/login.js
+++ clock-app/login.js
@@ -1,5 +1,5 @@
 // got help from chatGPT to create this login.js 
 
 document.getElementById("loginBtn").addEventListener("click", () => {
-    window.location.href = "calendar.html";
+    window.location.href = "clock.html";
 });
