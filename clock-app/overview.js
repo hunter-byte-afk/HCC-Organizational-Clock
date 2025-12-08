0290d1d1c99baa24590ab56b82b9d2cf62bb6fc2
VictoriaTrev <vrtrevin@nmsu.edu>
2025-12-07 21:03:20 -0700

    body class updates
    - overview class update
    - adding calendar to the overview page
    - adding x out buttons for the tasks
    - adjusting colors and orders of the overview page

--- clock-app/overview.js
+++ clock-app/overview.js
@@ -1,3 +1,24 @@
+//Clear Button Code
+var myNodeList = document.getElementsByTagName("LI");
+
+for (var i = 0; i < myNodeList.length; i++){
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+for (var i= 0; i < close.length; i++){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+//Task Entry Code
 function newElement(){
     var li = document.createElement("li");  //Creates new list element (new row for the list)
     var inputValue = document.getElementById("newTask").value;   //Retrieves what user entered
@@ -13,4 +34,17 @@ function newElement(){
     }
 
     document.getElementById("newTask").value = "";   //clears input after entry
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (var i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+        }
+    }
 }
