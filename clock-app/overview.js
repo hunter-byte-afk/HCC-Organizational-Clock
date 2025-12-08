43a4e889c3e8d8c7f14ce59e272b50d603112417
VictoriaTrev <vrtrevin@nmsu.edu>
2025-12-07 17:32:56 -0700

    task list
    code for adding the tasks, no css

--- clock-app/overview.js
+++ clock-app/overview.js
@@ -1,15 +1,15 @@
 function newElement(){
-    var list = document.createElement("list");  //Creates new list element (new row for the list)
+    var li = document.createElement("li");  //Creates new list element (new row for the list)
     var inputValue = document.getElementById("newTask").value;   //Retrieves what user entered
     var text = document.createTextNode(inputValue);     //Enters text into the list from inputValue
 
-    list.appendChild(text);
+    li.appendChild(text);
 
     if(inputValue == ''){  //alerts user to enter a task
         alert("Please enter a task.");
     }
     else{   //enters task into list
-        document.getElementById("taskList").appendChild(list);
+        document.getElementById("taskList").appendChild(li);
     }
 
     document.getElementById("newTask").value = "";   //clears input after entry
